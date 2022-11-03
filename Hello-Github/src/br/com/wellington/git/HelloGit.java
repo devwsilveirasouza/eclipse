4979362fd82b4d7aae0d9632d9d4aa3e5b14adcd
Wellington Silveira de Souza <dev.wsilveirasouza@gmail.com>
2022-11-03 20:40:47 -0300

    Add files via upload
    Integrando Eclipse com GitHub

---
+++ Hello-Github/src/br/com/wellington/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.wellington.git;
+
+/**
+ * @author Wellington
+ * Exemplo de integração do Eclipse com GitHub
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GitHub");
+	}
+
+}
